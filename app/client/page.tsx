f95c36c67093b963b5823c23294a5eadce65604a
Ahmed <ahmedjader837@gmail.com>
2025-01-18 03:11:10 -0500

    fixed client page, and added chatbot to show up there as well.

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from 'react'
 import { useUser } from '@clerk/nextjs'
+import Hero from '@/components/chatbot.jsx';
 
 const ClientPage = () => {
     const { isLoaded, isSignedIn, user } = useUser()
@@ -11,6 +12,7 @@ const ClientPage = () => {
     return (
         <div className='h-full flex flex-col items-center justify-center text-2xl'>
             Hello {user.firstName}, welcome to clerk
+            <Hero />
         </div>
     )
 }
