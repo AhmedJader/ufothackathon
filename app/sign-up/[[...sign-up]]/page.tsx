18d864773520a47ff69e502448a5ae4922c3c741
Ahmed <ahmedjader837@gmail.com>
2025-01-18 03:54:45 -0500

    integrated logo with sign in and sign up and added styling

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,16 @@
-import { SignUp } from '@clerk/nextjs'
+import { SignIn, SignUp } from '@clerk/nextjs'
+import Image from 'next/image'
+import logo from "@/public/AI_Generated_Logo_2025-01-18_676a4f25-bc8c-4d7c-90b7-01823d67f9bd.png"
 
 export default function Page() {
-  return <div className='flex items-center justify-center h-full'>
-      <SignUp afterSignOutUrl='/'/>
+  return (
+    <div className='flex h-full justify-center'> {/* Reduced negative gap */}
+      <div className='flex items-center justify-center'>
+        <Image src={logo} alt="Logo" className='sizeshrink' /> {/* Adjust size as needed */}
+      </div>
+      <div className='flex items-center justify-center'>
+        <SignUp afterSignOutUrl='/' />
+      </div>
     </div>
+  )
 }
